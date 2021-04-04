a34a0622527871a0011514f47357de3964b3665b
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 05:52:11 -0400

    ad

--- script/data.js
+++ script/data.js
@@ -1,12 +1,12 @@
 const data = [{
     id:1,
     src:"https://homepagemodel.glitch.me/",
-    log:".../coolcat.png",
+    log:"../coolcat.png",
     mainsite:"https://www.thejazzloft.org/",
     prom1:"https://www.bilibili.com/",
-    promIM:".../bili.png",
+    promIM:"../bili.png",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
-    prom2IM:".../jazz.png"
+    prom2IM:"../jazz.png"
 }]
 
 export default data;
