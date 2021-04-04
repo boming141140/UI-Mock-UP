0cea9dcc8269300ceffe8b021187883c346c9983
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 06:43:17 -0400

    sadd

--- script/data.js
+++ script/data.js
@@ -1,12 +1,12 @@
 const data = [{
     id:1,
     src:"https://homepagemodel.glitch.me/",
-    log:"../img/coolcat.jpg",
+    log:"img/coolcat.jpg",
     mainsite:"https://www.thejazzloft.org/",
     prom1:"https://www.bilibili.com/",
-    promim:"../img/bili.jpg",
+    promim:"img/bili.jpg",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
-    prom2im:"../img/jazz.jpg"
+    prom2im:"img/jazz.jpg"
 }];
 
 export default data;
