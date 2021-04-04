5def7d66fc3f64ae3ac5af510c967d1ed2d2e6b4
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 06:19:23 -0400

    sda

--- script/data.js
+++ script/data.js
@@ -7,6 +7,6 @@ const data = [{
     promim:"../img/bili.jpg",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
     prom2im:"../img/jazz.jpg"
-}]
+}];
 
 export default data;
