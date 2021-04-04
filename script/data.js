8bf2d89c9d29bb2943a5244436680de88afcadb9
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 06:48:11 -0400

    sas

--- script/data.js
+++ script/data.js
@@ -6,7 +6,6 @@ const data = [{
     prom1:"https://www.bilibili.com/",
     promim:"../img/bili.jpg",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
-    prom2im:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fgithub.com%2FfreeCodeCamp%2Fdevdocs%2Fissues%2F1239&psig=AOvVaw1W6lvdfK_bgX5veSyaYrPi&ust=1617619513379000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDFrba05O8CFQAAAAAdAAAAABAD"
-}];
+    prom2im:"../img/jazz.jpg"
 
 export default data;
