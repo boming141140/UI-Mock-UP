662822a8af361596c6e5d2c8be27baa105927e22
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 06:55:34 -0400

    ASD
    DASD

--- script/data.js
+++ script/data.js
@@ -7,5 +7,5 @@ const data = [{
     promim:"../img/bili.JPG",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
     prom2im:"../img/jazz.JPG"
-
+}];
 export default data;
