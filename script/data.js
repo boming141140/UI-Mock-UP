1d23817e508a1f164049be753702f5e7c48198b4
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 06:53:35 -0400

    AS

--- script/data.js
+++ script/data.js
@@ -1,11 +1,11 @@
 const data = [{
     id:1,
     src:"https://homepagemodel.glitch.me/",
-    log:"../img/coolcat.jpg",
+    log:"../img/coolcat.JPG",
     mainsite:"https://www.thejazzloft.org/",
     prom1:"https://www.bilibili.com/",
-    promim:"../img/bili.jpg",
+    promim:"../img/bili.JPG",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
-    prom2im:"../img/jazz.jpg"
+    prom2im:"../img/jazz.JPG"
 
 export default data;
