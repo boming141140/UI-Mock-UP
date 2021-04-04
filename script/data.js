bea7e0bf661bebf36cfeecab3f23779bd3e26ae0
boming141140 <69752753+boming141140@users.noreply.github.com>
2021-04-04 05:55:07 -0400

    dsa

--- script/data.js
+++ script/data.js
@@ -4,9 +4,9 @@ const data = [{
     log:"../coolcat.png",
     mainsite:"https://www.thejazzloft.org/",
     prom1:"https://www.bilibili.com/",
-    promIM:"../bili.png",
+    promim:"../bili.png",
     prom2:"https://www.amersfoortjazz.nl/?gclid=CjwKCAjwx6WDBhBQEiwA_dP8raIfiDxArPCp2rQHIbLTdb4rrbaRf-y5WFM1ipD45QIl5wenhK9-fhoCZ1MQAvD_BwE",
-    prom2IM:"../jazz.png"
+    prom2im:"../jazz.png"
 }]
 
 export default data;
